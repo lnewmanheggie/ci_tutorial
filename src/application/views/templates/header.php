83afe67cd3f02900cf7e178ae2674f0e6817e08c
lnewmanheggie <lnewmanheggie@gmail.com>
2021-04-08 12:39:56 -0500

    added navbar

--- src/application/views/templates/header.php
+++ src/application/views/templates/header.php
@@ -1,6 +1,36 @@
 <html>
-    <head>
-        <title>citutorial</title>
-        <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
-    </head>
-    <body>
+
+<head>
+    <title>citutorial</title>
+    <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
+</head>
+
+<body>
+    <!-- <nav class="navbar navbar-dark">
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="/">Blog</a>
+            </div>
+            <div id="navbar">
+                <ul class="nav navbar-nav">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav> -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="collapse navbar-collapse" id="navbarColor01">
+            <a class="navbar-brand" href="/">Blog</a>
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="<?php print base_url(); ?>">Home
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php/about">About</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
